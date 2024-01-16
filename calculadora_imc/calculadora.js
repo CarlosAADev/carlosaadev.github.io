8e5409f1335be492057fc8adc2f3bf4b7a7027bc
CarlosAADev <156712208+CarlosAADev@users.noreply.github.com>
2024-01-16 20:36:09 +0100

    Commit Calculadora

--- calculadora_imc/calculadora.js
+++ calculadora_imc/calculadora.js
@@ -3,7 +3,41 @@ document.querySelector("#calcular").addEventListener("click", function(){
     const altura = document.querySelector("#altura").value;
     const imc = calcularIMC(peso, altura);
 
-    document.querySelector("#resultado").innerHTML=imc.toFixed(1);
+    let clasificacion_peso;
+    let color;
+    let resultado = imc.toFixed(1);
+
+    if (resultado < 18.5){
+        //Bajo peso
+        clasificacion_peso = "Bajo peso";
+        color = "#1e91d0";
+    }else if (resultado >= 18.5 && resultado <= 24.9){
+        //Adecuado
+        clasificacion_peso = "Adecuado";
+        color ="#59b5e8";
+    }else if(resultado >= 25 && resultado <= 29.9){
+        //Sobrepeso
+        clasificacion_peso = "Sobrepeso";
+        color ="#2fa147";
+    }else if(resultado >= 30 && resultado <= 34.9){
+        //Obesidad 1
+        clasificacion_peso = "Obesidad 1";
+        color ="#f4cd34";
+    }else if(resultado >= 35 && resultado <= 39.9){
+        //Obesidad 2
+        clasificacion_peso = "Obesidad 2";
+        color ="#f7a717";
+    }else if(resultado >= 40){
+        //Obesidad Mórbida
+        clasificacion_peso = "Obesidad mórbida";
+        color ="#d4212b";
+    }
+
+    document.querySelector("#resultado").innerHTML=resultado + " - " + clasificacion_peso;
+
+    document.querySelector("#resultado").style.backgroundColor = color;
+    
+    //document.querySelector("#resultado").innerHTML=imc.toFixed(1) + "-" + clasificacion_peso;
 }); 
 
 
